f83d9535cb63486dbdb87532346a04456601c368
Lynder063 <lynderjekokot@gmail.com>
2024-08-24 21:03:42 +0200

    Update

--- src/components/Infobox.jsx
+++ src/components/Infobox.jsx
@@ -2,10 +2,24 @@ import PropTypes from "prop-types";
 
 function Infobox({ children }) {
   return (
-    <div className="p-4 m-4 bg-white rounded-lg shadow-md">
-      <div>{children}</div>
+    <div className="flex flex-row justify-center">
+      <div
+        className="p-4 m-4 text-center bg-white rounded-lg 
+                      w-full           /* Default: full width */
+                      sm:w-3/4         /* Small screens and up: 75% width */
+                      md:w-2/3         /* Medium screens and up: 66.67% width */
+                      lg:w-1/2         /* Large screens and up: 50% width */
+                      xl:w-1/3         /* Extra large screens and up: 33.33% width */
+                      "
+      >
+        {children}
+      </div>
     </div>
   );
 }
+
+Infobox.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Infobox;
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,18 +5,13 @@ function Topbar(props) {
   return (
     <div>
       <div className="flex fixed top-0 left-0 flex-row grid-cols-7 px-2 py-4 w-full align-baseline bg-white shadow-md">
-        <Link to="/" className="basis-1/12">
+        <Link to="/" className="basis-2/12">
           <span className="h-2 material-symbols-outlined">
             arrow_back_ios_new
           </span>
         </Link>
-        <div className="basis-11/12">
-          {" "}
-          <div className="flex flex-row align-baseline">
-            <div className="text-gray-400 basis-1/4">{props.number}</div>
-            <div className="text-center text-md basis-3/4">{props.title}</div>
-          </div>
-        </div>
+        <div className="text-gray-400 basis-2/12">{props.number}</div>
+        <div className="justify-start text-md basis-8/12">{props.title}</div>
       </div>
     </div>
   );
