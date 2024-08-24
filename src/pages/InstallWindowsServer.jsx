f3e3748875dffd1cbcaab3de8f00e4de60315781
Lynder063 <lynderjekokot@gmail.com>
2024-08-24 17:14:44 +0200

    update

--- src/pages/InstallWindowsServer.jsx
+++ src/pages/InstallWindowsServer.jsx
@@ -1,5 +1,22 @@
+import Infobox from "../components/Infobox";
+import Topbar from "../components/Topbar";
+
 function InstallWindowsServer() {
-  return;
+  return (
+    <div>
+      <Topbar number="#1" title="Instalace Windows serveru"></Topbar>
+      <Infobox>
+        Name se musí rovnat určité kovenci pojmenovaní, která je ve stylu
+        <br />
+        <strong>PS2-(váš login)-Win_server_2019</strong>
+        <br />
+      </Infobox>
+
+      <Infobox>
+        Nesmíme zapomenout uložit VM na disk E: do složky PTPS a do svého loginu
+      </Infobox>
+    </div>
+  );
 }
 
 export default InstallWindowsServer;
