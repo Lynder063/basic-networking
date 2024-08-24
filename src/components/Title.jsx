e9983e091b0646c2b1adf09b292ce4ab8e92f2b3
Lynder063 <lynderjekokot@gmail.com>
2024-08-24 20:18:09 +0200

    Homepage responsivity done!

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,10 +6,10 @@ function Title(props) {
     <div>
       <Link
         to={props.link}
-        className="grid grid-cols-4 p-4 m-4 bg-white rounded-lg shadow-md"
+        className="flex flex-row p-4 m-4 bg-white rounded-lg shadow-md"
       >
-        <div className="text-xl text-gray-400">{props.number}</div>
-        <div className="col-span-3 text-lg font-semibold">{props.title}</div>
+        <div className="text-xl text-gray-400 basis-3/12">{props.number}</div>
+        <div className="text-lg font-semibold basis-9/12">{props.title}</div>
       </Link>
     </div>
   );
--- src/components/Titlebar.jsx
+++ src/components/Titlebar.jsx
@@ -8,11 +8,6 @@ const Titlebar = () => {
           Základy počítačových sítí
         </div>
       </div>
-      <div className="top-0 left-0 w-full bg-white">
-        <div className="px-2 py-4 w-full text-lg font-semibold text-center bg-white rounded-lg shadow-md">
-          Základy počítačových sítí
-        </div>
-      </div>
     </div>
   );
 };
