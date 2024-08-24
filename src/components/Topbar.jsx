eecaaa14b0d26e37da024dd4f977543001d26cdf
Lynder063 <lynderjekokot@gmail.com>
2024-08-24 20:33:49 +0200

    Topbar responsivity #1

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -4,30 +4,17 @@ import { Link } from "react-router-dom";
 function Topbar(props) {
   return (
     <div>
-      <div className="grid fixed top-0 left-0 grid-cols-7 px-2 py-4 w-full bg-white shadow-md">
-        <Link to="/">
-          <span className="col-span-1 text-lg text-center material-symbols-outlined">
+      <div className="flex fixed top-0 left-0 flex-row grid-cols-7 px-2 py-4 w-full align-baseline bg-white shadow-md">
+        <Link to="/" className="basis-1/12">
+          <span className="h-2 material-symbols-outlined">
             arrow_back_ios_new
           </span>
         </Link>
-        <div className="col-span-5 col-start-3 text-lg">
-          <div className="grid grid-cols-7">
-            <div className="col-span-1 text-gray-400">{props.number}</div>
-            <div className="col-span-6">{props.title}</div>
-          </div>
-        </div>
-      </div>
-
-      <div className="grid top-0 left-0 grid-cols-7 px-2 py-4 w-full bg-white shadow-md">
-        <Link to="/">
-          <span className="col-span-1 text-lg text-center material-symbols-outlined">
-            arrow_back_ios_new
-          </span>
-        </Link>
-        <div className="col-span-5 col-start-3 text-lg">
-          <div className="grid grid-cols-7">
-            <div className="col-span-1 text-gray-400">{props.number}</div>
-            <div className="col-span-6">{props.title}</div>
+        <div className="basis-11/12">
+          {" "}
+          <div className="flex flex-row align-baseline">
+            <div className="text-gray-400 basis-1/4">{props.number}</div>
+            <div className="text-center text-md basis-3/4">{props.title}</div>
           </div>
         </div>
       </div>
