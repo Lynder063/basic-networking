35e8c956942546c5fae768b81c6b6c0df0f87e8f
Lynder063 <lynderjekokot@gmail.com>
2024-08-25 12:49:39 +0200

    deploy routing fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PageError from "./pages/PageError";
 import InstallWindowsServer from "./pages/InstallWindowsServer";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> },
-  {
-    path: "/instalace-windows-serveru",
-    element: <InstallWindowsServer />,
-  },
-]);
+// Nastavte basename na jméno vašeho repozitáře na GitHubu
+const basename = "/nazev-repozitare"; // Změňte na skutečný název vašeho repozitáře
+
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Homepage /> },
+    {
+      path: "/instalace-windows-serveru",
+      element: <InstallWindowsServer />,
+    },
+    { path: "*", element: <PageError /> }, // Pro neexistující cesty
+  ],
+  { basename } // Přidá základní název do každé cesty
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
